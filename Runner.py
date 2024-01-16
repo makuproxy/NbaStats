727c796846fc2e87eca63777ca417111a2e202eb
Kiefer Fernandez Valladolid <Kiefer.Fernandez@tranzact.net>
2024-01-15 19:15:29 -0500

    To add support for env variables, fixing issues to save into google 'batch_update' applied, removing json file with credentials

--- Runner.py
+++ Runner.py
@@ -6,14 +6,34 @@ import time
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import numpy as np
+import os
+from google.oauth2 import service_account
+from dotenv import load_dotenv
+import json
 
+process_start_time = time.time()
 
-def scrape_and_save(urls, sheet_suffix, team_data=None):
+load_dotenv()
+
+# Constants
+GSHEET_NBA_MAKU_CREDENTIALS = os.getenv("GSHEET_NBA_MAKU_CREDENTIALS")
+GSHEET_NBA_MAKU_FOLDER_ID = os.getenv("GSHEET_NBA_MAKU_FOLDER_ID")
+FILENAME_OUTPUT = os.getenv("FILENAME_OUTPUT")
+FORMAT_OUTPUT_TYPE = os.getenv("FORMAT_OUTPUT_TYPE") or 'excel'
+
+def load_credentials():
+    # Load and return Google Sheets credentials
+    # credentials = ServiceAccountCredentials.from_json_keyfile_name('./rapid-stage-642-94546a81c2dc.json', scope)
+    scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+    credentials_dict = json.loads(GSHEET_NBA_MAKU_CREDENTIALS)
+    return ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
+
+def scrape_data(urls, sheet_suffix, team_data=None):
     # If team_data is not provided, initialize an empty dictionary
     team_data = team_data or {}
 
     # Start the timer
-    start_time = time.time()    
+    url_start_time = time.time()
 
     # Iterate through each URL
     for url in urls:
@@ -51,7 +71,6 @@ def scrape_and_save(urls, sheet_suffix, team_data=None):
                 team_df = pd.read_html(StringIO(str(main_elements[index + 1])))[0]
                 break
 
-
         # Add the DataFrame to the dictionary with the team name as the key
         if team_name and team_df is not None:
             if team_name not in team_data:
@@ -61,16 +80,83 @@ def scrape_and_save(urls, sheet_suffix, team_data=None):
                 team_data[team_name] = pd.concat([team_data[team_name], team_df])
 
     # Stop the timer
-    end_time = time.time()
+    url_end_time = time.time()
 
     # Calculate the total time taken
-    total_time = end_time - start_time
+    url_total_time = url_end_time - url_start_time
 
     # Print the total time taken
-    print(f"Total time taken: {total_time:.2f} seconds")
+    print(f"Total time taken: {url_total_time:.2f} seconds for {sheet_suffix}")
 
     # Return the updated team_data
     return team_data
+
+def save_excel(data, filename):
+    # Create an Excel file with all data in separate sheets using openpyxl engine
+    with pd.ExcelWriter(f'{filename}.xlsx', engine='openpyxl') as writer:
+        for team_name, df in data.items():
+            sheet_name = f"{team_name}"  # No need for suffix here
+            df.to_excel(writer, sheet_name=sheet_name, index=False)
+
+def save_sheets(data, folder_id, sheet_name):
+    # Set up Google Sheets credentials
+    credentials = load_credentials()
+    gc = gspread.authorize(credentials)
+    google_folder_id = folder_id
+
+    existing_spread_sheets = gc.list_spreadsheet_files(folder_id=google_folder_id)
+    spread_sheet_exists = any(sheet_name == sheet['name'] for sheet in existing_spread_sheets)
+
+    if spread_sheet_exists:
+        spread_sheet_main = gc.open(sheet_name, google_folder_id)
+    else:
+        spread_sheet_main = gc.create(sheet_name, google_folder_id)
+
+    # Clear all sheets in the spreadsheet
+    for sheet in spread_sheet_main.worksheets():
+        sheet.clear()
+
+    # DELETE all sheets in the spreadsheet
+    # for sheet in spread_sheet_main.worksheets():
+    #     if sheet.title != "BaseNoDelete":
+    #         spread_sheet_main.del_worksheet(sheet)
+
+    spread_sheet_helper = None
+    update_requests = []
+
+    for team_name, df in data.items():
+        # Get or create a worksheet with the team name
+        try:
+            spread_sheet_helper = spread_sheet_main.worksheet(title=team_name)
+        except gspread.exceptions.WorksheetNotFound:
+            spread_sheet_helper = spread_sheet_main.add_worksheet(title=team_name, rows=800, cols=50)
+
+        update_requests.append({
+            'updateCells': {
+                'range': {
+                    'sheetId': spread_sheet_helper.id,
+                },
+                'fields': 'userEnteredValue',
+            }
+        })
+
+        # Update values in the worksheet
+        update_requests.append({
+            'updateCells': {
+                'range': {
+                    'sheetId': spread_sheet_helper.id,
+                },
+                'fields': 'userEnteredValue',
+                'rows': [{'values': [{'userEnteredValue': {'stringValue': str(value)}} for value in row]} for row in
+                         [df.columns.tolist()] + df.replace({np.nan: None}).values.tolist()],
+            }
+        })
+
+    batch_update_values_request_body = {
+        'requests': update_requests
+    }
+
+    spread_sheet_main.batch_update(batch_update_values_request_body)
 
 if __name__ == "__main__":
     # URLs for schedule
@@ -90,82 +176,27 @@ if __name__ == "__main__":
         "https://basketball.realgm.com/nba/teams/Atlanta-Hawks/1/Stats/2024/Averages/All/points/All/desc/1/Regular_Season",
         "https://basketball.realgm.com/nba/teams/Boston-Celtics/2/Stats/2024/Averages/All/points/All/desc/1/Regular_Season"
     ]
-    
+
     # Scrape and save schedule data
-    team_data = scrape_and_save(schedule_urls, "_RS")
+    schedule_data = scrape_data(schedule_urls, "_RS")
 
-    # Scrape and save stats data, passing the existing team_data
-    team_data = scrape_and_save(stats_urls, "_ST", team_data)
+    # Scrape and save stats data, passing the existing schedule_data
+    stats_data = scrape_data(stats_urls, "_ST", schedule_data)
 
+    # Save data based on the output type
+    if FORMAT_OUTPUT_TYPE == 'excel':
+        save_excel(stats_data, FILENAME_OUTPUT)
+    elif FORMAT_OUTPUT_TYPE == 'sheets':
+        save_sheets(stats_data, GSHEET_NBA_MAKU_FOLDER_ID, FILENAME_OUTPUT)
 
-    # Create a function to save the data based on the output_type
-    def save_data(data, gsName):
+    process_end_time = time.time()
 
-        # Set up Google Sheets credentials
-        scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']        
-        credentials = ServiceAccountCredentials.from_json_keyfile_name('./rapid-stage-642-94546a81c2dc.json', scope)        
-        gc = gspread.authorize(credentials)
-        googleFolderId = '1Xowlegsahvlo628xT1TGw5UF70OW8awb'
+    # Calculate the total time taken
+    process_total_time = process_end_time - process_start_time
 
-        output_type = 'sheets'
-
-
-        if output_type == 'excel':
-            # Create an Excel file with all data in separate sheets using openpyxl engine
-            with pd.ExcelWriter('output_teams_combined.xlsx', engine='openpyxl') as writer:
-                for team_name, df in team_data.items():            
-                    sheet_name = f"{team_name}"  # No need for suffix here
-                    df.to_excel(writer, sheet_name=sheet_name, index=False)
-        elif output_type == 'sheets':            
-            existing_spreadSheets = gc.list_spreadsheet_files(folder_id=googleFolderId)
-            spreadSheet_exists = any(gsName == sheet['name'] for sheet in existing_spreadSheets)
-
-            if spreadSheet_exists:                
-                spreadSheetMain = gc.open(gsName, googleFolderId)
-            else:                
-                spreadSheetMain = gc.create(gsName, googleFolderId)
-            
-            # Clear all sheets in the spreadsheet
-            for sheet in spreadSheetMain.worksheets():
-                sheet.clear()
-
-            # DELETE all sheets in the spreadsheet
-            # for sheet in spreadSheetMain.worksheets():
-            #     if sheet.title != "BaseNoDelete":
-            #         spreadSheetMain.del_worksheet(sheet)
-
-            spreadSheetHelper = None
-
-            for team_name, df in data.items():
-                # Get or create a worksheet with the team name                
-                try:                    
-                    spreadSheetHelper = spreadSheetMain.worksheet(title=team_name)
-                except gspread.exceptions.WorksheetNotFound:
-                    spreadSheetHelper = spreadSheetMain.add_worksheet(title=team_name, rows=800, cols=50)
-
-                # # Update the worksheet with the data                
-                # print(spreadSheetHelper.title)                
-                values = [df.columns.values.tolist()] + df.replace({np.nan: None}).values.tolist()
-                # print(values)
-                # print("11111")
-                # print("11111")
-                # print("11111")
-                # print("11111")
-                # print("11111")
-                # print("11111")
-                spreadSheetHelper.update('A1', values)
-
-                # # Execute the batch update
-                # TODO: BEGIN
-                # Check how to work with batch_update See: 
-                # https://docs.gspread.org/en/latest/user-guide.html#getting-all-values-from-a-row-or-a-column
-                # https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/batchUpdate 
-                # Other alternatives which are not native
-                # https://github.com/dgilman/gspread_asyncio
-                # More info:
-                # https://medium.com/hacktive-devs/gspread-automate-google-sheet-with-python-dc1fa7c65c21
-                # spreadSheetMain.batch_update(cell_range, values)  
-                # TODO: END
-                
-       
-    save_data(team_data, 'demoCreation')
+    # Print the total time taken
+    print("*************************************")
+    print("*************************************")
+    print(f"Total time taken: {process_total_time:.2f} seconds")
+    print("*************************************")
+    print("*************************************")
